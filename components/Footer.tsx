f7b1ff26bcb1265c5a991c754d218edb2942d8a3
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2023-04-17 18:17:28 +0100

    Commit four

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,40 +6,60 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className="container">
         <div>
-          <h2>Donors</h2>
-          <Image src="/donor.jpg" width="200" height="150" alt="" />
+          <div>
+            <h4>Our Partner</h4>
+            <span className="dash"></span>
+            <Image src="/donor.jpg" width="200" height="150" alt="" />
+          </div>
         </div>
         <div>
+
+
           <div>
+
+
             <div>
-              <h4>ADDRESS</h4>
+              <h4>Address</h4>
+              <span className="dash"></span>
               <p>500 Terry Francine St. San Francisco, CA 94158</p>
             </div>
             <div>
-              <h4>PHONE</h4>
+              <h4>Phone</h4>
+              <span className="dash"></span>
               <p>+233 20 334 443</p>
             </div>
             <div>
-              <h4>EMAIL</h4>
+              <h4>Email</h4>
+              <span className="dash"></span>
               <p>info@nehopcosa.org</p>
             </div>
           </div>
+
+
           <div>
             <a href="">
               <i className="fa fa-facebook fa-lg"></i>
+              Facebook
             </a>
             <a href="">
               <i className="fa fa-twitter fa-lg"></i>
+              Twitter
             </a>
             <a href="">
               <i className="fa fa-instagram fa-lg"></i>
+              Instagram
             </a>
             <a href="">
               <i className="fa fa-youtube fa-lg"></i>
+              Youtube
             </a>
           </div>
+
+
         </div>
       </div>
+
+
       <div className="container">&copy; 2023 NEHOPCOSA</div>
     </footer>
   );
