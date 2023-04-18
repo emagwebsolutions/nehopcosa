0319acb4ce254c856419706816f2652f4f715575
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2023-04-18 11:42:28 +0100

    Commit six

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,23 +5,28 @@ import Link from 'next/link';
 const Nav = () => {
   return (
     <nav className={styles.nav}>
+      <Image src="/menu.jpg" alt="" className="menu" width="70" height="70" />
       <div className="container">
-        <Image alt="Logo" src="/logo.jpg" width="70" height="70" />
+        <div>
+          <Image alt="Logo" src="/logo.jpg" width="70" height="70" />
 
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/">About</Link>
-          </li>
-          <li>
-            <Link href="/">Blog</Link>
-          </li>
-          <li>
-            <Link href="/">Contact</Link>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">About</Link>
+            </li>
+            <li>
+              <Link href="/">Blog</Link>
+            </li>
+            <li>
+              <Link href="/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div></div>
       </div>
     </nav>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Slide from '@/components/Slide';
 import styles from '@/styles/Home.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -31,6 +32,132 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className={styles.whoweare}>
+        <div className="container">
+          <div>
+            <Image src="/whoweare.jpg" width="560" height="400" alt="" />
+          </div>
+          <div>
+            <h1>Who we are?</h1>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio
+              asperiores assumenda officia commodi itaque beatae modi!
+              Voluptatum, beatae doloribus? Quibusdam voluptatem illo fuga sit,
+              placeat atque suscipit veritatis iusto.
+            </p>
+
+            <Link href="" className="button">
+              Learn More
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.help}>
+        <div>
+          <div>
+
+            <h2>They Need Your Help</h2>
+            <p>Support a cause you care about</p>
+
+            <Link href="" className="button-outline">
+              Learn More
+            </Link>
+     
+          </div>
+        </div>
+
+        <div>
+          <div>JOIN US TO HELP THE POOR AND NEEDY IN SOCIETY</div>
+        </div>
+      </section>
+
+
+
+<section className={styles.blog}>
+
+  <h4>Our Blog</h4>
+  <h2>Recent From Blog</h2>
+
+  <div className="container">
+
+
+  <div>
+      <Image width="230" height="170" alt="" src="/blog.jpg" />
+      <div>
+      <h3>Sponsor a child</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae delectus veniam, unde eos tenetur enim beatae, odio voluptatem 
+      </p>
+
+      <Link href="" className="button">Learn More</Link>
+      </div>
+    </div>
+
+
+
+    <div>
+      <Image width="230" height="170" alt="" src="/blog.jpg" />
+      <div>
+      <h3>Sponsor a child</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae delectus veniam, unde eos tenetur enim beatae, odio voluptatem 
+      </p>
+
+      <Link href="" className="button">Learn More</Link>
+      </div>
+    </div>
+
+
+
+    <div>
+      <Image width="230" height="170" alt="" src="/blog.jpg" />
+      <div>
+      <h3>Sponsor a child</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae delectus veniam, unde eos tenetur enim beatae, odio voluptatem 
+      </p>
+
+      <Link href="" className="button">Learn More</Link>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+    <div>
+      <Image width="230" height="170" alt="" src="/blog.jpg" />
+      <div>
+      <h3>Sponsor a child</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae delectus veniam, unde eos tenetur enim beatae, odio voluptatem 
+      </p>
+
+      <Link href="" className="button">Learn More</Link>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+</section>
+
+
+
     </>
   );
 }
