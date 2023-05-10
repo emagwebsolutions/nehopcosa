170b6557d296140fa918dbd78fed32f2b15897d0
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2023-05-10 15:23:53 +0100

    Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
-import Slide from '@/components/Slide';
-import styles from '@/styles/Home.module.scss';
-import Image from 'next/image';
-import Link from 'next/link';
+
 import { useGetpostsQuery } from '@/store/fetchData';
 import { useEffect } from 'react';
 
 import {
-  homepage,
-  selectPageState,
-  selectPostState,
-  selectProfileState
+  homepage
+
 } from '@/store/store';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { builder } from '@/client/client';
+import { useDispatch} from 'react-redux';
+
 
 function Home() {
   const { data } = useGetpostsQuery('');
@@ -30,6 +25,11 @@ function Home() {
 
     return (
       <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
        THIS IS HOME PAGE
       </>
     );
